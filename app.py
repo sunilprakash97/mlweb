a81af22e5ccb2cfd1b538bbe788c74d90a2eeaec
Sunilprakash Jaisankar <78901480+sunilprakash97@users.noreply.github.com>
2022-09-20 19:58:36 -0400

    ML Web App

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template,Response
 import cv2
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_url_path = '', 
+            static_folder = 'static')
 
 def faceBox(faceNet, frame):
     frameHeight = frame.shape[0]
@@ -18,7 +20,15 @@ def faceBox(faceNet, frame):
             x2 = int(detection[0, 0, i, 5] * frameWidth)
             y2 = int(detection[0, 0, i, 6] * frameHeight)
             bboxs.append([x1,y1,x2,y2])
-            cv2.rectangle(frame, (x1,y1),(x2,y2),(0,255,0),  2 )
+            cv2.rectangle(frame, (x1,y1),(x2,y2),(50, 168, 151),  2)
+            cv2.line (frame, (x1,y1), (x1+30, y1), (50, 168, 151), 6)
+            cv2.line (frame, (x1,y1), (x1, y1+30), (50, 168, 151), 6)
+            cv2.line (frame, (x2,y1), (x2-30, y1), (50, 168, 151), 6)
+            cv2.line (frame, (x2,y1), (x2, y1+30), (50, 168, 151), 6)
+            cv2.line (frame, (x1,y2), (x1+30, y2), (50, 168, 151), 6)
+            cv2.line (frame, (x1,y2), (x1, y2-30), (50, 168, 151), 6)
+            cv2.line (frame, (x2,y2), (x2-30, y2), (50, 168, 151), 6)
+            cv2.line (frame, (x2,y2), (x2, y2-30), (50, 168, 151), 6)
     return frame, bboxs
     
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
@@ -30,11 +40,12 @@ ageProto = "age.prototxt"
 ageModel = "age_model.caffemodel"
 genderProto = "gender.prototxt"
 genderModel = "gender_model.caffemodel"
+
 ageNet = cv2.dnn.readNet(ageModel, ageProto)
 genderNet = cv2.dnn.readNet(genderModel, genderProto)
 faceNet = cv2.dnn.readNet(faceModel, faceProto)
 
-camera=cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
 def generate_frames():
     while True:
@@ -54,8 +65,8 @@ def generate_frames():
                 age = ageList[agePreds[0].argmax()]
                 label = "{},{}".format(gender, age)
                 cv2.putText(frameFace, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
-            ret,buffer=cv2.imencode('.jpg',frameFace)
-            frame=buffer.tobytes()
+            ret,buffer = cv2.imencode('.jpg',frameFace)
+            frame = buffer.tobytes()
         yield(b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -68,4 +79,4 @@ def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
-    app.run(debug=True)    
+    app.run(debug = True)    
